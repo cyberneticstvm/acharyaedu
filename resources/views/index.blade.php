5c19e67c1700086651c78be41c27b718176c05ac
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-03-22 19:44:28 +0530

    small updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
             <div class="col-xl-6 col-lg-7 col-md-12">
                 <h4>Become Master</h4>
                 <h2 class="bg-text color-900">A better <span class="text-gradient fw-bold">learning</span> future starts here</h2>
-                <p class="color-500 lead mb-4">Build a beautiful, modern website with flexible Bootstrap components built from scratch.</p>
+                <p class="color-500 lead mb-4">Build a better and beautiful career with Acharya Educations by learning from our industry expert faculties.</p>
                 <a href="/register" class="btn px-4 py-3 lift btn-primary text-uppercase rounded-pill">Register</a>
                 <a href="/signin" class="btn px-4 py-3 lift btn-dark text-uppercase rounded-pill">Sign In</a>
             </div>
