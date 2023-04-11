00736683aef9c21638ebd1d8dec4e0fe0a9d1c51
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-04-11 20:50:36 +0530

    small updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,10 @@
     <div class="container">
 
         <div class="row g-3 justify-content-between align-items-center">
-            <div class="col-xl-6 col-lg-7 col-md-12">
+            <div class="col-xl-6 col-lg-7 col-md-12 text-center">
                 <!--<h4>Become Master</h4>
                 <h2 class="bg-text color-900">A better <span class="text-gradient fw-bold">learning</span> future starts here</h2>-->
-                <div class="text-center"><img src="{{ asset('assets/images/logo/acharya-blue.png') }}" class="img-fluid" alt="Acharya"/></div>
+                <img src="{{ asset('assets/images/logo/acharya-blue.png') }}" class="img-fluid" alt="Acharya"/>
                 <!--<p class="color-500 lead mb-4">Build a better and beautiful career with Acharya Educations by learning from our industry expert faculties.</p>-->
                 <a href="/register" class="btn px-4 py-3 lift btn-primary text-uppercase rounded-pill">Register</a>
                 <a href="/signin" class="btn px-4 py-3 lift btn-dark text-uppercase rounded-pill">Sign In</a>
