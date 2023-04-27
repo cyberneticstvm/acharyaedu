f74435e1d7d0976f27344ba25d8307fea0ada893
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-04-27 18:37:07 +0530

    small updates

--- resources/views/student/result.blade.php
+++ resources/views/student/result.blade.php
@@ -43,18 +43,20 @@
                                             <hr>
                                         @empty
                                         @endforelse
-                                        <div class="">
-                                            <button data-bs-toggle="collapse" data-bs-target="#correctanswer" class="btn btn-primary">Show Correct Answer</button>
-                                            <div class="expl text-success quest collapse mt-1" id="correctanswer">
-                                                {{ $quest->question->options()->where('option_id', $quest->question->correct_option)->value('option_name') }}
+                                        <div class="row">
+                                            <div class="col">
+                                                <button data-bs-toggle="collapse" data-bs-target="#correctanswer" class="btn btn-primary">Show Correct Answer</button>
+                                                <div class="expl text-success quest collapse mt-1" id="correctanswer">
+                                                    {{ $quest->question->options()->where('option_id', $quest->question->correct_option)->value('option_name') }}
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="mt-1">
-                                            <button data-bs-toggle="collapse" data-bs-target="#explanation" class="btn btn-primary">Show Explanation</button>
-                                            <div class="expl text-info quest collapse mt-1" id="explanation">
-                                                {{ $quest->question->explanation }}
+                                            <div class="col">
+                                                <button data-bs-toggle="collapse" data-bs-target="#explanation" class="btn btn-primary">Show Explanation</button>
+                                                <div class="expl text-info quest collapse mt-1" id="explanation">
+                                                    {{ $quest->question->explanation }}
+                                                </div>
                                             </div>
-                                        </div>
+                                        </div>                                        
                                     </div>
                                     @empty
                                     @endforelse
