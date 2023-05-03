82119a39e82ddeb4904350a23d7e113e17e33838
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-05-03 12:41:37 +0530

    small updates

--- public/assets/js/chart.js
+++ public/assets/js/chart.js
@@ -17,7 +17,7 @@ $(function(){
           data: cor
         }],
           chart: {
-          type: 'donut',
+          type: 'bar',
           height: 350
         },
         colors: ['#ff0000', '#228B22'],
@@ -70,7 +70,7 @@ $(function(){
           data: cor
         }],
           chart: {
-          type: 'bar',
+          type: 'pie',
           height: 350
         },
         colors: ['#ff0000', '#228B22'],
