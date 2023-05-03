e5ab815a56f7df0e22ba746cfb755e55e5c62fe1
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-05-03 10:32:50 +0530

    small updates

--- resources/views/admin/student/result.blade.php
+++ resources/views/admin/student/result.blade.php
@@ -55,19 +55,6 @@
                                     @empty
                                     @endforelse
                                 </div>
-                                @forelse($exam->scores as $key => $quest)
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="text-success answer quest collapse mt-1 p-3" id="correctanswer{{$quest->id}}">
-                                            {!! $quest->question->options()->where('option_id', $quest->question->correct_option)->value('option_name') !!}
-                                        </div>
-                                        <div class="text-dark answer quest collapse mt-1 p-3" id="explanation{{$quest->id}}">
-                                            {!! ($quest->question->explanation) ? $quest->question->explanation : 'Explanation will be added soon.' !!}
-                                        </div>
-                                    </div>
-                                </div>
-                                @empty
-                                @endforelse
                                 <!-- Include optional progressbar HTML -->
                                 <div class="progress">
                                     <div class="progress-bar" role="progressbar" style="width: 0%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
