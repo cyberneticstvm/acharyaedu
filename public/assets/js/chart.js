3a26814c4d634d7ca5abb485e216a27be6e1625d
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-05-03 09:58:44 +0530

    small updates

--- public/assets/js/chart.js
+++ public/assets/js/chart.js
@@ -54,6 +54,59 @@ $(function(){
         chart.render();
       });
 
+      $.getJSON('/studentperfchart/'+seid, function(response){
+        var cor=[]; var wro=[]; var sub=[];
+        for(var i=0; i<response.length; i++){
+            cor.push(response[i]['correct']);
+            wro.push(response[i]['wrong']);
+            sub.push(response[i]['sname']);
+        }
+        var options = {
+            series: [{
+            name: 'Wrong',
+            data: wro
+          }, {
+            name: 'Correct',
+            data: cor
+          }],
+            chart: {
+            type: 'bar',
+            height: 350
+          },
+          colors: ['#ff0000', '#228B22'],
+          plotOptions: {
+            bar: {
+              horizontal: false,
+              columnWidth: '55%',
+              endingShape: 'rounded',
+            },
+          },
+          dataLabels: {
+            enabled: false
+          },
+          xaxis: {
+            categories: sub,
+          },
+          yaxis: {
+            title: {
+              text: 'Score'
+            }
+          },
+          fill: {
+            opacity: 1
+          },
+          tooltip: {
+            y: {
+              formatter: function (val) {
+                return val + " Score"
+              }
+            }
+          }
+        };
+        var chart = new ApexCharts(document.querySelector("#studPerfChart1"), options);
+        chart.render();
+      });
+
       $.getJSON('/studentperfchartall', function(response){
         var cor=[]; var wro=[]; var sub=[];
         for(var i=0; i<response.length; i++){
