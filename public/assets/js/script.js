288df2504a6b2e86b8f8f0b969d71dc3e4f09c43
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-05-03 08:56:08 +0530

    small updates

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -54,7 +54,7 @@ $(function(){
 });
 
 function clearAnswer(dis){
-    var chk = dis.parent().parent().find('.quest :visible').find(".radanswer").data('chk');
+    var chk = dis.closest('.quest :visible').find(".radanswer").data('chk');
     alert(chk)
     $("input[name='"+chk+"']").prop('checked', false);
     //var radioValue = $("input[name='"+chk+"']:checked").val();
