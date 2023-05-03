c972d3256db0803678672e24f19fc5d74c250356
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-05-03 12:58:17 +0530

    small updates

--- public/assets/js/chart.js
+++ public/assets/js/chart.js
@@ -54,54 +54,31 @@ $(function(){
       chart.render();
     });
 
-    $.getJSON('/studentperfchartall', function(response){
-      var cor=[]; var wro=[]; var sub=[];
+    $.getJSON('/studentperfchartall', function(response){alert(response)
+      var da = []; var sub=[];
       for(var i=0; i<response.length; i++){
-          cor.push(response[i]['correct']);
-          wro.push(response[i]['wrong']);
-          sub.push(response[i]['sname']);
+        da.push((parseInt(response[i]['correct'])/(parseInt(response[i]['correct'])+parseInt(response[i]['wrong'])))*100);
+        sub.push(response[i]['sname']);
       }
       var options = {
-          series: [{
-          name: 'Wrong',
-          data: wro
-        }, {
-          name: 'Correct',
-          data: cor
-        }],
+          series: da,
           chart: {
           type: 'pie',
-          height: 350
+          width: 380
         },
+        labels:sub,
         colors: ['#ff0000', '#228B22'],
-        plotOptions: {
-          bar: {
-            horizontal: false,
-            columnWidth: '55%',
-            endingShape: 'rounded',
-          },
-        },
-        dataLabels: {
-          enabled: false
-        },
-        xaxis: {
-          categories: sub,
-        },
-        yaxis: {
-          title: {
-            text: 'Score'
-          }
-        },
-        fill: {
-          opacity: 1
-        },
-        tooltip: {
-          y: {
-            formatter: function (val) {
-              return val + " Score"
+        responsive: [{
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: 'bottom'
             }
           }
-        }
+        }]
       };
       var chart = new ApexCharts(document.querySelector("#studPerfChartAll"), options);
       chart.render();
