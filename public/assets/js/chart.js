b3c44328d7039fb4de2c9743472c53511226d1de
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-05-03 10:11:01 +0530

    small updates

--- public/assets/js/chart.js
+++ public/assets/js/chart.js
@@ -1,60 +1,7 @@
 $(function(){
     "use strict"
     var seid = $("#student_exam_id").val();
-    $.getJSON('/studentperfchartall', function(response){
-      var cor=[]; var wro=[]; var sub=[];
-      for(var i=0; i<response.length; i++){
-          cor.push(response[i]['correct']);
-          wro.push(response[i]['wrong']);
-          sub.push(response[i]['sname']);
-      }
-      var options = {
-          series: [{
-          name: 'Wrong',
-          data: wro
-        }, {
-          name: 'Correct',
-          data: cor
-        }],
-          chart: {
-          type: 'bar',
-          height: 350
-        },
-        colors: ['#ff0000', '#228B22'],
-        plotOptions: {
-          bar: {
-            horizontal: false,
-            columnWidth: '55%',
-            endingShape: 'rounded',
-          },
-        },
-        dataLabels: {
-          enabled: false
-        },
-        xaxis: {
-          categories: sub,
-        },
-        yaxis: {
-          title: {
-            text: 'Score'
-          }
-        },
-        fill: {
-          opacity: 1
-        },
-        tooltip: {
-          y: {
-            formatter: function (val) {
-              return val + " Score"
-            }
-          }
-        }
-      };
-      var chart = new ApexCharts(document.querySelector("#studPerfChartAll"), options);
-      chart.render();
-    });
-    
-      $.getJSON('/studentperfchart/'+seid, function(response){ alert(response)
+      $.getJSON('/studentperfchart/'+seid, function(response){
         var cor=[]; var wro=[]; var sub=[];
         for(var i=0; i<response.length; i++){
             cor.push(response[i]['correct']);
@@ -104,6 +51,59 @@ $(function(){
           }
         };
         var chart = new ApexCharts(document.querySelector("#studPerfChart"), options);
+        chart.render();
+      });
+
+      $.getJSON('/studentperfchartall', function(response){
+        var cor=[]; var wro=[]; var sub=[];
+        for(var i=0; i<response.length; i++){
+            cor.push(response[i]['correct']);
+            wro.push(response[i]['wrong']);
+            sub.push(response[i]['sname']);
+        }
+        var options = {
+            series: [{
+            name: 'Wrong',
+            data: wro
+          }, {
+            name: 'Correct',
+            data: cor
+          }],
+            chart: {
+            type: 'bar',
+            height: 350
+          },
+          colors: ['#ff0000', '#228B22'],
+          plotOptions: {
+            bar: {
+              horizontal: false,
+              columnWidth: '55%',
+              endingShape: 'rounded',
+            },
+          },
+          dataLabels: {
+            enabled: false
+          },
+          xaxis: {
+            categories: sub,
+          },
+          yaxis: {
+            title: {
+              text: 'Score'
+            }
+          },
+          fill: {
+            opacity: 1
+          },
+          tooltip: {
+            y: {
+              formatter: function (val) {
+                return val + " Score"
+              }
+            }
+          }
+        };
+        var chart = new ApexCharts(document.querySelector("#studPerfChartAll"), options);
         chart.render();
       });
 });
