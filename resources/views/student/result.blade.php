95ef48dbb0c98a0c381e1ce2aab9e60020e02b3b
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-05-18 11:27:01 +0530

    small updates

--- resources/views/student/result.blade.php
+++ resources/views/student/result.blade.php
@@ -33,9 +33,9 @@
                                     Question {!! $c++.'. '. nl2br($quest->question->question) !!}<br><br>
                                     @forelse($quest->question->options as $key1 => $opt)
                                         @php $color = ""; @endphp
-                                        @if($quest->correct_option == $opt->option_id && $quest->selected_option == $quest->question->correct_option)
+                                        @if($quest->correct_option == $opt->option_id && $quest->answer == 1)
                                             @php $color = "text-success fw-bold"; @endphp
-                                        @elseif($quest->selected_option == $opt->option_id && $quest->selected_option != $quest->question->correct_option)
+                                        @elseif($quest->answer == 0 && $quest->selected_option == $opt->option_id && $quest->selected_option != NULL)
                                             @php $color = "text-danger fw-bold"; @endphp                                          
                                         @endif                                        
                                         <span class="{{ $color }}">{!! nl2br($opt->option_name) !!}</span><br>
