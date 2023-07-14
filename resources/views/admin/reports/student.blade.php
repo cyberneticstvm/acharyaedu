e2ab1988cae9d1f0472279054fe2b51a24ffe5cf
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-07-14 08:59:44 +0530

    small updates

--- resources/views/admin/reports/student.blade.php
+++ resources/views/admin/reports/student.blade.php
@@ -44,17 +44,50 @@
                         <p class="fw-bold">Admission Date: <span class="text-primary">{{ date('d/M/Y', strtotime($student->admission_date)) }}</span></p>
                     </div>
                     <div class="col">
-                        <p class="fw-bold">Batche(s): <span class="text-primary">{{ $batches }}</span></p>
+                        <p class="fw-bold">Batches: <span class="text-primary">{{ $batches }}</span></p>
                     </div>
                 </div>
                 @endif
                 <div class="row mt-5">
                     <div class="col-12">
+                        <h5>Admission Fee & Leave Details</h5>
                         <div class="table-responsive">
                             <table class="table table-striped table-bordered">
                                 <thead class="thead-light">
                                     <th>Batch</th>
-                                    <th>Fee</th>
+                                    <th>Fee Advance</th>
+                                    <th>Fee Balance</th>
+                                    <th>Fee Balance Paid</th>
+                                    <th>Date Paid</th>
+                                    <th>Leave Count</th>
+                                </thead>
+                                <tbody>
+                                    @forelse($records as $key => $student)
+                                    <tr>
+                                        <td>{{ $student->batch()->find($student->batch)->name }}</td>
+                                        <td>{{ $student->studentname->admission_fee_advance }}</td>
+                                        <td>{{ $student->studentname->admission_fee_balance }}</td>
+                                        <td>{{ ($student->studentname->balance_received == 0 && $student->studentname->admission_fee_balance > 0) ? 'Not Paid' : 'Paid' }}</td>
+                                        <td>{{ $student->studentname->admission_date->format('d/M/Y') }}</td>
+                                        <td>{{ $student->studentname->attendances()->where('batch', $student->batch)->where('leave', 1)->count('leave') }}</td>
+                                    </tr>
+                                    @empty
+                                    @endforelse
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-12">
+                        <h5>Batch Fee Details</h5>
+                        <div class="table-responsive">
+                            <table class="table table-striped table-bordered">
+                                <thead class="thead-light">
+                                    <th>Batch</th>
+                                    <th>Fee Advance</th>
+                                    <th>Fee Balance</th>
+                                    <th>Fee Balance Paid</th>
                                     <th>Fee Month</th>
                                     <th>Date Paid</th>
                                     <th>Status</th>
@@ -65,6 +98,8 @@
                                     <tr>
                                         <td>{{ $record->batch()->find($record->batch)->name }}</td>
                                         <td>{{ $fee->value('fee') }}</td>
+                                        <td>{{ $fee->value('fee_balance') }}</td>
+                                        <td>{{ ($fee->value('fee_pending') == 0 && $fee->value('fee_balance') > 0) ? 'Not Paid' : 'Paid' }}</td>
                                         <td>{{ $fee->value('fee_month')  }} / {{ $fee->value('fee_year') }}</td>
                                         <td>{{ ($fee->value('paid_date')) ? date('d/M/Y', strtotime($fee->value('paid_date'))) : '' }}</td>
                                         <td class="text-center">{!! ($fee->value('fee') > 0) ? "<i class='fa fa-check text-success'>" : "<i class='fa fa-times text-danger'>" !!}</td>
