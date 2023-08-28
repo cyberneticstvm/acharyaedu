5fe235e8728de5d782bf6dcecaa7c51fef7b4913
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-08-28 13:00:48 +0530

    small updates

--- resources/views/onam-celeb-2023-video.blade.php
+++ resources/views/onam-celeb-2023-video.blade.php
@@ -9,10 +9,14 @@
                     <span class="pre">Video</span>
                     <h2 class="title">Onam Celebration 2023</h2>
                 </div>
-                <video width="320" height="240" controls>
-                    <source src="{{ asset('storage/ceremony/Acharya_Onam_Celebration_2023.mp4') }}" type="video/mp4">
-                    Your browser does not support the video tag.
-                </video>
+                <div class="row">
+                    <div class="col text-center">
+                        <video width="100%" controls>
+                            <source src="{{ asset('storage/ceremony/Acharya_Onam_Celebration_2023.mp4') }}" type="video/mp4">
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
