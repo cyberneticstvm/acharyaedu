a9dec94ed058f4ad7678a4bdfac426b73af22dda
Cybernetics <mail@cybernetics.me>
2024-02-01 10:24:41 +0530

    small updates

--- resources/views/student/question-general.blade.php
+++ resources/views/student/question-general.blade.php
@@ -14,7 +14,8 @@
                 @forelse($questions as $key => $question)
                 <p>Subject: {{ $question->subject->name }}</p>
                 <p>Question: {{ $question->question }}</p>
-                <p>Answer: {{ $question->answer }}</p>
+                <p><a data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false">Show Answer</a></p>
+                <p class="collapse" id="collapseExample">Answer: {{ $question->answer }}</p>
                 <p>Explanation: {{ $question->explanation }}</p>
                 @empty
                 @endforelse
