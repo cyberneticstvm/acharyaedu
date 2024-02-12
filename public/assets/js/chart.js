85cfa57b226f8ff2657bddda46ab0cf529e171f8
Cybernetics <mail@cybernetics.me>
2024-02-12 14:05:17 +0530

    small updates

--- public/assets/js/chart.js
+++ public/assets/js/chart.js
@@ -1,85 +1,85 @@
-$(function(){
-    "use strict"
-    var seid = $("#student_exam_id").val(); var type = $("#student_exam_type").val();
-    $.getJSON('/studentperfchart/'+seid+'/'+type, function(response){
-      var cor=[]; var wro=[]; var sub=[];
-      for(var i=0; i<response.length; i++){
-          cor.push(response[i]['correct']);
-          wro.push(response[i]['wrong']);
-          sub.push(response[i]['sname']);
-      }
-      var options = {
-          series: [{
-          name: 'Wrong',
-          data: wro
-        }, {
-          name: 'Correct',
-          data: cor
-        }],
-          chart: {
-          type: 'bar',
-          height: 350
+$(function () {
+  "use strict"
+  var seid = $("#student_exam_id").val(); var type = $("#student_exam_type").val();
+  $.getJSON('/studentperfchart/' + seid + '/' + type, function (response) {
+    var cor = []; var wro = []; var sub = [];
+    for (var i = 0; i < response.length; i++) {
+      cor.push(response[i]['correct']);
+      wro.push(response[i]['wrong']);
+      sub.push(response[i]['sname']);
+    }
+    var options = {
+      series: [{
+        name: 'Wrong',
+        data: wro
+      }, {
+        name: 'Correct',
+        data: cor
+      }],
+      chart: {
+        type: 'bar',
+        height: 350
+      },
+      colors: ['#ff0000', '#228B22'],
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: '55%',
+          endingShape: 'rounded',
         },
-        colors: ['#ff0000', '#228B22'],
-        plotOptions: {
-          bar: {
-            horizontal: false,
-            columnWidth: '55%',
-            endingShape: 'rounded',
-          },
-        },
-        dataLabels: {
-          enabled: false
-        },
-        xaxis: {
-          categories: sub,
-        },
-        yaxis: {
-          title: {
-            text: 'Score'
-          }
-        },
-        fill: {
-          opacity: 1
-        },
-        tooltip: {
-          y: {
-            formatter: function (val) {
-              return val + " Score"
-            }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      xaxis: {
+        categories: sub,
+      },
+      yaxis: {
+        title: {
+          text: 'Score'
+        }
+      },
+      fill: {
+        opacity: 1
+      },
+      tooltip: {
+        y: {
+          formatter: function (val) {
+            return val + " Score"
           }
         }
-      };
-      var chart = new ApexCharts(document.querySelector("#studPerfChart"), options);
-      chart.render();
-    });
-
-    $.getJSON('/studentperfchartall', function(response){
-      var da = []; var sub=[];
-      for(var i=0; i<response.length; i++){
-        da.push((parseInt(response[i]['correct'])/(parseInt(response[i]['correct'])+parseInt(response[i]['wrong'])))*100);
-        sub.push(response[i]['sname']);
       }
-      var options = {
-          series: da,
+    };
+    var chart = new ApexCharts(document.getElementById("studPerfChart"), options);
+    chart.render();
+  });
+
+  $.getJSON('/studentperfchartall', function (response) {
+    var da = []; var sub = [];
+    for (var i = 0; i < response.length; i++) {
+      da.push((parseInt(response[i]['correct']) / (parseInt(response[i]['correct']) + parseInt(response[i]['wrong']))) * 100);
+      sub.push(response[i]['sname']);
+    }
+    var options = {
+      series: da,
+      chart: {
+        type: 'pie',
+        width: '100%'
+      },
+      labels: sub,
+      responsive: [{
+        breakpoint: 480,
+        options: {
           chart: {
-          type: 'pie',
-          width: '100%'
-        },
-        labels:sub,
-        responsive: [{
-          breakpoint: 480,
-          options: {
-            chart: {
-              width: '100%'
-            },
-            legend: {
-              position: 'bottom'
-            }
+            width: '100%'
+          },
+          legend: {
+            position: 'bottom'
           }
-        }]
-      };
-      var chart = new ApexCharts(document.querySelector("#studPerfChartAll"), options);
-      chart.render();
-    });
+        }
+      }]
+    };
+    var chart = new ApexCharts(document.querySelector("#studPerfChartAll"), options);
+    chart.render();
+  });
 });
