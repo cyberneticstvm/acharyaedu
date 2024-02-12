d749c1acca20ef089bf76639bd378fd45388449f
Cybernetics <mail@cybernetics.me>
2024-02-12 13:59:04 +0530

    small updates

--- public/admin-assets/js/acharya-charts.js
+++ public/admin-assets/js/acharya-charts.js
@@ -4,7 +4,6 @@ $(function () {
   var type = $("#student_exam_type").val();
   var ctx1 = document.getElementById("studentPerformanceChart");
   $.getJSON('/studentperfchart/' + seid + '/' + type, function (response) {
-    alert('dsf')
     var cor = []; var wro = []; var sub = [];
     for (var i = 0; i < response.length; i++) {
       cor.push(response[i]['correct']);
