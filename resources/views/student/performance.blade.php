e2e8070f6e3d54b76dfb9e0378203e07abfb8988
Cybernetics <mail@cybernetics.me>
2024-02-12 13:38:11 +0530

    small updates

--- resources/views/student/performance.blade.php
+++ resources/views/student/performance.blade.php
@@ -1,12 +1,14 @@
-@extends("admin.base")
+@extends("student.base")
 @section("content")
 <div class="row">
     <div class="col-xl-12 col-lg-12 col-md-12">
         <div class="card">
             <div class="card-header pb-0 text-left bg-transparent">
                 <div class="row">
-                    <div class="col"><h3 class="font-weight-bolder text-primary text-gradient">{{ $student->name }}'s Exam Performance ({{ $exam->studentexam->exam->name }})</h3></div>
-                </div>                
+                    <div class="col">
+                        <h3 class="font-weight-bolder text-primary text-gradient">{{ $student->name }}'s Exam Performance ({{ $exam->studentexam->exam->name }})</h3>
+                    </div>
+                </div>
             </div>
             <div class="card-body">
                 <div class="row">
