e6825ed81ebc2677806a1a02f039f6afa14205ff
Cybernetics <mail@cybernetics.me>
2024-02-13 15:04:00 +0530

    small updates

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -726,6 +726,8 @@ class StudentController extends Controller
             'correct_answer' => 'required|numeric',
             'wrong_answer' => 'required|numeric'
         ]);
+        dd($request);
+        die;
         try {
             $exam = OfflineExam::findOrFail($request->exam_id);
             $studentexam = StudentOfflineExam::findOrFail($id);
