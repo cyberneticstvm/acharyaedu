66f39c9a1a2dc9dfbb0dd390236094ed3e31242e
Cybernetics <mail@cybernetics.me>
2024-04-01 09:31:15 +0530

    small updates

--- app/Models/StudentOfflineExam.php
+++ app/Models/StudentOfflineExam.php
@@ -20,4 +20,30 @@ class StudentOfflineExam extends Model
     {
         return $this->belongsTo(Student::class, 'student_id', 'id');
     }
+
+    public function getPerformance()
+    {
+        $performance = "";
+        $percentage = $this->performance;
+        switch ($percentage):
+            case $percentage < 40:
+                $performance = "Below Average";
+                break;
+            case $percentage >= 40 && $percentage < 50:
+                $performance = "Average";
+                break;
+            case $percentage >= 50 && $percentage < 60:
+                $performance = "Above Average";
+                break;
+            case $percentage >= 50 && $percentage < 60:
+                $performance = "Above Average";
+                break;
+            case $percentage >= 60 && $percentage < 75:
+                $performance = "Good";
+                break;
+            default:
+                $performance = "Excellent";
+        endswitch;
+        return $performance;
+    }
 }
