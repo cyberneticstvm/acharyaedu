5e387498bbce3b8b71ed4975bd3bf301296bada4
Vijoy Sasidharan <mail@cybernetics.me>
2024-08-02 09:15:10 +0400

    small updates

--- resources/views/admin/base.blade.php
+++ resources/views/admin/base.blade.php
@@ -28,9 +28,10 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
   <!-- Nucleo Icons -->
   <link href="{{ asset('admin-assets/css/nucleo-icons.css') }}" rel="stylesheet" />
-  <link href="{{ asset('admin-assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+  <!--<link href="{{ asset('admin-assets/css/nucleo-svg.css') }}" rel="stylesheet" />-->
   <!-- Font Awesome Icons -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.1.0/css/v4-shims.min.css" crossorigin="anonymous"></script>
+  <link src="{{ asset('admin-assets/css/font-awesome.min.css') }}" rel="stylesheet">
+  </link>
   <link href="{{ asset('admin-assets/css/nucleo-svg.css') }}" rel="stylesheet" />
   <!-- CSS Files -->
   <link id="pagestyle" href="{{ asset('admin-assets/css/soft-ui-dashboard.css?v=1.0.7') }}" rel="stylesheet" />
