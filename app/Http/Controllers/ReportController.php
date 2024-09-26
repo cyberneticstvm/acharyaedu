9354021cb1e9cf1d6d558e0d712ea7184d0f6c59
Cybernetics <mail@cybernetics.me>
2024-09-26 14:07:08 +0530

    small updates

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -141,7 +141,8 @@ class ReportController extends Controller
         $days = cal_days_in_month(CAL_GREGORIAN, $request->month, $request->year);
         $record = StudentBatch::where('batch', $request->batch)->where('cancelled', 0)->where('student', 384)->first();
         //for ($i = 1; $i <= $days; $i++):
-        echo $record->studentname()->find($record->student)->attendances()->whereDay('date', 26)->whereMonth('date', 9)->whereYear('date', 2024)->selectRaw("CASE WHEN present = 1 THEN 'P' WHEN `leave` = 1 THEN 'L' ELSE 'A' END AS atype");
+        $records = $record->studentname()->find($record->student)->attendances()->whereDay('date', 26)->whereMonth('date', 9)->whereYear('date', 2024)->selectRaw("CASE WHEN present = 1 THEN 'P' WHEN `leave` = 1 THEN 'L' ELSE 'A' END AS atype");
+        dd($records);
         //endfor;
         die;
         return view('admin.reports.attendance', compact('records', 'batches', 'months', 'years', 'inputs', 'days'));
