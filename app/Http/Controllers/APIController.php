28f2f11b4174ed74615df4c786fd0fc6799dfbac
Cybernetics <mail@cybernetics.me>
2024-11-21 12:20:40 +0530

    small updates

---
+++ app/Http/Controllers/APIController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class APIController extends Controller
+{
+    function getAuthUser(Request $request)
+    {
+        $user = User::find(1);
+        return response()->json([
+            'status' => true,
+            'user' => $user,
+        ], 200);
+    }
+}
