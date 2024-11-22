09779a0cd22e3c5cee33c8613e2644f1c7013168
Cybernetics <mail@cybernetics.me>
2024-11-22 14:18:17 +0530

    small updates

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -34,7 +34,7 @@ class APIController extends Controller
                     'status' => false,
                     'user' => $user,
                     'message' => 'Invalid Credentials',
-                    'email' => $request->all()['email'],
+                    'email' => $request->all(),
                 ], 404);
             endif;
         } else {
